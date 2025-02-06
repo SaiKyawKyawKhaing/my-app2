00dfcf395273ae4001c1465b15d86b88a5637524
kyawkyawkhaing@abank.com.mm <kyawkyawkhaing@abank.com.mm>
2025-02-06 23:08:51 +0630

    change 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,14 @@
-import { useEffect,useState } from "react";
-import { useFetchJson } from "../utils/util";
+const data = require("../data/data.json"); // Require works even without import config
 
 export default function Home() {
-  // const data = useFetchJson("/index.json");
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""; // Needed for GitHub Pages
-    fetch(`${basePath}/data.json`) // Use the correct path
-      .then((response) => response.json())
-      .then((result) => setData(result))
-      .catch(console.error);
-  }, []);
   return (
     <div>
       <h1>Users</h1>
       <ul>
-        <li>123456789</li>
+        {data.map((user) => (
+          <li key={user.id}>{user.name} - {user.role}</li>
+        ))}
       </ul>
-      <ul>{data.map((u) => <h1 key={u.id}>{u.name} - {u.role}</h1>)}</ul>
     </div>
   );
 }
